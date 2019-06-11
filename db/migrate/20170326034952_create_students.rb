540e207f66f5923d759d67debc0380e8b18dea68
Paulo Borba <pauloesb@gmail.com>
2019-06-11 14:30:26 -0300

    Added Name column to Students table

--- db/migrate/20170326034952_create_students.rb
+++ db/migrate/20170326034952_create_students.rb
@@ -2,6 +2,7 @@ class CreateStudents < ActiveRecord::Migration[5.2]
   def change
     create_table :students do |t|
       t.integer :cpf
+      t.string :name
       t.string :email
       t.string :phone
       t.date :date_birth
