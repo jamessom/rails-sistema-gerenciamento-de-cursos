8242b9597b9949981067a468684c9fb7592c8ee6
Paulo Borba <pauloesb@gmail.com>
2019-06-11 13:31:21 -0300

    Fix: Changed Courses to Subjects in all mention files

---
+++ app/controllers/api/v1/subjects_controller.rb
@@ -0,0 +1,49 @@
+class Api::V1::SubjectsController < ApplicationController
+  before_action :set_subject, only: [:show, :update, :destroy]
+
+  def index
+    render json: Subject.all
+  end
+
+  def show
+    render json: @subject
+  end
+
+  def create
+    @subject = Subject.new(subject_params)
+
+    if @subject.save
+      render json: @subject, status: :created
+    else
+      default_error_render
+    end
+  end
+
+  def update
+    if @subject.update(subject_params)
+      render json: @subject, status: :ok
+    else
+      default_error_render
+    end
+  end
+
+  def destroy
+    @subject.destroy
+    :no_content
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_subject
+      @subject = Subject.find(params[:id])
+    end
+
+    def default_error_render
+      render json: @subject.errors, status: :unprocessable_entity
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def subject_params
+      params.require(:subject).permit(:name, :requiriment, :workload, :price)
+    end
+end
