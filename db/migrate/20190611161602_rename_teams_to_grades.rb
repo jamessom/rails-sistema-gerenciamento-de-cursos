dde9a8ac5f6bf6a884db7f2f72fd09c1efca2308
Paulo Borba <pauloesb@gmail.com>
2019-06-11 14:29:10 -0300

    Changed filename to the correct one

--- db/migrate/20190611161602_rename_teams_to_grades.rb
+++ db/migrate/20190611161602_rename_teams_to_grades.rb
@@ -1,7 +1,8 @@
-class RenameTableTeamsToGrades < ActiveRecord::Migration[5.2]
+class RenameTeamsToGrades < ActiveRecord::Migration[5.2]
   def change
     rename_table :teams, :grades
     rename_column :enrollments, :teams_id, :grades_id
+    rename_column :grades, :trainers_id, :teachers_id
     remove_column :grades, :workload, :integer
   end
 end
