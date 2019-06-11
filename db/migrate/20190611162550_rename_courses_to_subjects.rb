5ae0a4d2d1b1d0e61f0b9dd1fbdb379495736c58
Paulo Borba <pauloesb@gmail.com>
2019-06-11 14:30:46 -0300

    Fix: Fixed name requiriment to requirement

--- db/migrate/20190611162550_rename_courses_to_subjects.rb
+++ db/migrate/20190611162550_rename_courses_to_subjects.rb
@@ -2,5 +2,6 @@ class RenameCoursesToSubjects < ActiveRecord::Migration[5.2]
   def change
     rename_table :courses, :subjects
     rename_column :grades, :courses_id, :subjects_id
+    rename_column :subjects, :requiriment, :requirement
   end
 end
