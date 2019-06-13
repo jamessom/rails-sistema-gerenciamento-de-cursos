3b0b52649b0355fcef3a9b91d4fde4e8f54c7689
Paulo Borba <pauloesb@gmail.com>
2019-06-13 19:02:02 -0300

    This commit fixes #16
    - Fixed model subject and teacher
    - Added Annotate to these models

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,15 @@
-class Course < ApplicationRecord
+# == Schema Information
+#
+# Table name: subjects
+#
+#  id          :bigint           not null, primary key
+#  name        :string
+#  requirement :string
+#  workload    :integer
+#  price       :decimal(, )
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Subject < ApplicationRecord
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,3 +1,15 @@
-class Teachers < ApplicationRecord
+# == Schema Information
+#
+# Table name: teachers
+#
+#  id           :bigint           not null, primary key
+#  hour_value   :integer
+#  certificates :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  employer_id  :bigint
+#
+
+class Teacher < ApplicationRecord
     belongs_to :employer
 end
