499c3e47784c4e98be5598c538d4601dd5908687
Paulo Borba <pauloesb@gmail.com>
2019-06-13 20:06:46 -0300

    Fixing plurality of foreign keys

--- db/migrate/20190611162550_rename_courses_to_subjects.rb
+++ db/migrate/20190611162550_rename_courses_to_subjects.rb
@@ -1,7 +1,7 @@
 class RenameCoursesToSubjects < ActiveRecord::Migration[5.2]
   def change
     rename_table :courses, :subjects
-    rename_column :grades, :courses_id, :subjects_id
+    rename_column :grades, :course_id, :subject_id
     rename_column :subjects, :requiriment, :requirement
   end
 end
